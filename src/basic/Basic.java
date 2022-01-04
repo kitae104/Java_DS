1b31735ac10ebe52783e564d58f4bc3bb6a23e85
kitae104 <aqua0405@gmail.com>
2022-01-05 02:34:01 +0900

    수정

---
+++ src/basic/Basic.java
@@ -0,0 +1,27 @@
+package basic;
+
+public class Basic
+{
+
+	public static void main(String[] args)
+	{
+		// 1, A, 전우진, 여
+		// 1, A, 허유진, 여
+		// 1, B, 이수, 남
+		// 1, B, 이인범, 남
+		// 1, B, 조화영, 여
+		// 1, C, 김지은, 여
+		// 1, C, 이슬기, 여
+		// 1, C, 주호정, 여
+		
+		
+		
+		// 1. 클래스 - 출력 
+		// 2. Scanner 이용해서 외부에서 2명 정보 입력 
+		
+		
+		
+		
+	}
+
+}
