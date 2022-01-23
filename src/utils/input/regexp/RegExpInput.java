08d3ceb9e13871b59d025f3499390f14267558c9
kitae104 <aqua0405@gmail.com>
2022-01-23 11:29:51 +0900

    Update RegExpInput.java

--- src/utils/input/regexp/RegExpInput.java
+++ src/utils/input/regexp/RegExpInput.java
@@ -8,11 +8,11 @@ public class RegExpInput
 
 	public static void main(String[] args)
 	{
-//		String regExp = "\\s*[a-zA-Z]+";  // 공백 
-		String regExp = "\\s*[a-zA-Z]+";  // 공백 
+		String regExp = "\\s*[a-zA-Z]+";  // 정규식 
+		String regExp2 = "\\s";  // 공백 
 		String strTest = "{([[]])}";
 		String strTest2 = "A * ( B + C / D )";
-		StringTokenizer st = new StringTokenizer(strTest2, "{}()[]+*/-}", true);
+		StringTokenizer st = new StringTokenizer(strTest2, "{}()[]+*/-}", true); // 공백 고려
 		
 		int i = 1;
 		while(st.hasMoreTokens())
